0671711059de9c19d04f4f71a86e35627d2653cb
Jaison George <jaisong87@gmail.com>
2012-04-19 23:08:44 -0400

    More inputs

---
+++ src/learners/Pairs.java
@@ -0,0 +1,27 @@
+
+package learners;
+import java.util.Vector;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author prasad
+ */
+public class Pairs {
+        String character;
+            Vector<String> states;
+            
+         public String getChar ()
+         {
+             return character;
+         }
+         
+         public Vector<String> getStates()
+         {
+             return states;
+         }
+}
